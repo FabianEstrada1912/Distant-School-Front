9fdbd26a7f7a26c645e69012878e60b3c5bd6baa
Silica <191272@ids.upchiapas.edu.mx>
2021-11-08 09:08:31 -0600

    diseño

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,32 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ChatComponent } from './pagesUser/chat/chat.component';
+import { ConfiguracionComponent } from './pagesUser/configuracion/configuracion.component';
+import { EditUserComponent } from './pagesUser/edit-user/edit-user.component';
+import { PerfilComponent } from './pagesUser/perfil/perfil.component';
+import { SolicitarComponent } from './pagesUser/solicitar/solicitar.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path:'configuracion',
+    component:ConfiguracionComponent,
+    children :[
+      {
+        path:'',
+        component:PerfilComponent,
+      },{
+        path:'editarUsuario',
+        component:EditUserComponent,
+      },{
+        path:'aceptacionAmigos',
+        component:SolicitarComponent,
+      },{
+        path:'creacion-grupos',
+        component:ChatComponent,
+      }
+    ]
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ConfiguracionComponent } from './pagesUser/configuracion/configuracion.component';
+import { EditUserComponent } from './pagesUser/edit-user/edit-user.component';
+import { SolicitarComponent } from './pagesUser/solicitar/solicitar.component';
+import { ChatComponent } from './pagesUser/chat/chat.component';
+import { PerfilComponent } from './pagesUser/perfil/perfil.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ConfiguracionComponent,
+    EditUserComponent,
+    SolicitarComponent,
+    ChatComponent,
+    PerfilComponent
   ],
   imports: [
     BrowserModule,
